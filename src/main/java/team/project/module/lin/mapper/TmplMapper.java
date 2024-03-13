1da8932f053c92723540fdba76934cf9310bf867
lin <1980634531@qq.com>
2024-03-13 20:45:58 +0800

    第一次提交

---
+++ src/main/java/team/project/module/lin/mapper/TmplMapper.java
@@ -0,0 +1,17 @@
+package team.project.module.lin.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import team.project.module.lin.model.entity.TmplDO;
+
+import java.util.List;
+
+/* 封装对数据库的操作，是 sql 语句的直接映射 */
+@Mapper
+public interface TmplMapper extends BaseMapper<TmplDO> {
+
+    List<TmplDO> list(); /* 示例 */
+
+    /* 这里定义一些“只有本模块会使用”的方法，
+       等确定某个方法会被多个模块使用后，再把它提升到 global.mapper 包中 */
+}
---
+++ src/main/java/team/project/module/lin/util/TmplUtil.java
@@ -0,0 +1,7 @@
+package team.project.module.lin.util;
+
+/* 工具类 */
+public class TmplUtil {
+    /* 这里定义一些“只有本模块会使用”的方法，
+       等确定某个方法会被多个模块使用后，再把它提升到 global.util 包中 */
+}
