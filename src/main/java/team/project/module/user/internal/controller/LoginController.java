9a207d3bb2116f798c5e514bdad57b0452b11f0b
szdxdxdx <3510745357@qq.com>
2024-04-02 14:16:00 +0800

    Sa-Token 尝试使用 SaSession，修改注册和登录的入参校验

--- src/main/java/team/project/module/user/internal/controller/LoginController.java
+++ src/main/java/team/project/module/user/internal/controller/LoginController.java
@@ -2,21 +2,23 @@ package team.project.module.user.internal.controller;
 
 import cn.dev33.satoken.annotation.SaCheckLogin;
 import cn.dev33.satoken.stp.StpUtil;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import team.project.base.controller.Response;
 import team.project.base.service.status.ServiceStatus;
 import team.project.module.user.internal.model.view.UserInfoVO;
 import team.project.module.user.internal.service.LoginService;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Tag(name="登录")
 @RestController
@@ -30,21 +32,26 @@ public class LoginController {
     @PostMapping("/user/login/password")
     Object loginWithPassword(
 
-        @NotNull(message="学号/工号不能为空")
         @NotBlank(message="学号/工号不能为空")
         @Size(min=1, max=20, message="学号/工号的长度不合约束")
         @RequestParam("user_id") String userId,
 
+        @NotBlank(message="密码不能为空")
+        @Size(min=1, max=20, message="密码的长度不合约束")
+        @RequestParam("pwd") String password
 
-        @NotBlank @RequestParam("pwd")     String password
     ) {
         UserInfoVO userInfo = service.login(userId, password);
 
         StpUtil.login(userId);
-    /*  String token = StpUtil.getTokenValueByLoginId(userId);
-        StpUtil.getSessionByLoginId(StpUtil.getLoginId()).set("user_id", userId); */
+        String token = StpUtil.getTokenValueByLoginId(userId);
+        StpUtil.getSessionByLoginId(StpUtil.getLoginId()).set("user_id", userId);
 
-        return new Response<>(ServiceStatus.SUCCESS).statusText("登录成功").data(userInfo);
+        Map<String, Object> loginResult = new HashMap<>();
+        loginResult.put("user_info", userInfo);
+        loginResult.put("token", token);
+
+        return new Response<>(ServiceStatus.SUCCESS).statusText("登录成功").data(loginResult);
     }
 
     @Operation(summary="使用邮箱登录", hidden=true)
