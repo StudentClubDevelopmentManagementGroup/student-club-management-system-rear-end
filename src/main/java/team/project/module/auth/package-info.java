c8d0ee3e1c3fc0db9a597b2cccf9a88e4a68891d
szdxdxdx <3510745357@qq.com>
2024-05-03 15:54:35 +0800

    package-info

--- src/main/java/team/project/module/auth/package-info.java
+++ src/main/java/team/project/module/auth/package-info.java
@@ -1,11 +1,10 @@
 /**
- * <h3>介绍</h3>
- * <p>本模块配置 Sa-Token 框架</p>
- * <p></p>
- * <h3>注意</h3>
- * <p>使用 @SaCheckRole 注解鉴权时，不要直接用字符串写角色，而是用下方枚举类封装的常量
- * @see team.project.module.auth.export.model.enums.AuthRole 封装的角色枚举常量
- * </p>
- * <p></p>
+ * <h3> 介绍
+ * <p>  本模块配置 Sa-Token 框架
+ * <p>
+ * <h3> 注意
+ * <p>  使用 @SaCheckRole 注解鉴权时，不要直接用字符串写角色，而是用下方枚举类封装的常量
+ *      @see team.project.module.auth.export.model.enums.AuthRole 封装的角色枚举常量
+ * <p>
  */
 package team.project.module.auth;
