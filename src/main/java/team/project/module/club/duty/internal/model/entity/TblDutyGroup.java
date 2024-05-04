e0151a0738eb2c0b114f2960005319e45b25f59f
lin <1980634531@qq.com>
2024-05-04 19:19:01 +0800

    添加通过社团id查询社团基本信息的接口2.0

--- src/main/java/team/project/module/club/duty/internal/model/entity/TblDutyGroup.java
+++ src/main/java/team/project/module/club/duty/internal/model/entity/TblDutyGroup.java
@@ -1,12 +1,13 @@
 package team.project.module.club.duty.internal.model.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 @Data
 @TableName("tbl_user_club_duty_group")
 public class TblDutyGroup {
-    @TableField(value="id")          Long      id;
+    @TableId(value="id")             Long      id;
     @TableField(value="club_id")     Long      club_id;
     @TableField(value="member_id")   String    member_id;
     @TableField(value="name")        String    name;
