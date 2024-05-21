ed89af3d78da6024b55eea435bf1c2873554e78e
szdxdxdx <3510745357@qq.com>
2024-05-21 17:29:56 +0800

    简单的文本模板工具 TextTemplate

---
+++ src/main/java/team/example/util/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <h1> 介绍
+ * <p>  本包简单的示例代码，来介绍如何使用 {@link team.project.util} 包下的工具类
+ * <p>  本包下的所有代码与业务逻辑无关，仅作演示
+ * <p>
+ */
+package team.example.util;
--- src/main/java/team/project/util/package-info.java
+++ src/main/java/team/project/util/package-info.java
@@ -4,6 +4,11 @@
  * <ul>
  * <li> 带有自动过期功能的键值对容器
  *      {@link team.project.util.expiringmap.ExpiringMap ExpiringMap}
+ * <li> 简单的文本模板工具
+ *      {@link team.project.util.texttmpl.TextTemplate TextTemplate}
  * </ul>
+ * <p>  配备了简单的示例代码来介绍如何使用这些工具类，
+ * <br> {@link team.example.util}
+ * <p>
  */
 package team.project.util;
