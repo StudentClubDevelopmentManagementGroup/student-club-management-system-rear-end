f638eaacb48dba025c50701c768ad5c1feab3d51
lin <1980634531@qq.com>
2024-06-11 17:57:52 +0800

    lin负责的所有接口现已加权

--- src/main/java/team/project/module/club/duty/internal/controller/DutyController.java
+++ src/main/java/team/project/module/club/duty/internal/controller/DutyController.java
@@ -1,5 +1,7 @@
 package team.project.module.club.duty.internal.controller;
 
+import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.stp.StpUtil;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -13,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 import team.project.base.controller.response.Response;
 import team.project.base.model.view.PageVO;
 import team.project.base.service.status.ServiceStatus;
+import team.project.module.auth.export.model.enums.AuthRole;
+import team.project.module.auth.export.service.AuthServiceI;
 import team.project.module.club.duty.internal.model.entity.TblDutyCirculation;
 import team.project.module.club.duty.internal.model.entity.TblDutyGroup;
 import team.project.module.club.duty.internal.model.query.DutyGroupQO;
@@ -35,66 +39,100 @@ public class DutyController {
     @Autowired
     DutyCirculationService dutyCirculationService;
 
+    @Autowired
+    AuthServiceI authService;
+
     @Operation(summary = "添加小组成员")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/group/add")
     Object addDutyGroup(@Valid @RequestBody GroupMemberReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能添加小组成员");
+
         dutyGroupService.createDutyGroup(req.getClubId(), req.getMemberId(), req.getName());
         return new Response<>(ServiceStatus.SUCCESS).statusText("创建成功");
     }
 
     @Operation(summary = "删除小组成员")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/group/delete")
     Object deleteDutyGroup(@Valid @RequestBody GroupMemberReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能删除小组成员");
+
         dutyGroupService.deleteDutyGroup(req.getClubId(), req.getMemberId(), req.getName());
         return new Response<>(ServiceStatus.SUCCESS).statusText("删除成功");
     }
 
     @Operation(summary = "根据小组名称以及社团id，添加值日信息")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/create_by_group")
     Object createDutyByNameAndClubId(@Valid @RequestBody DutyInfoGroupReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能添加值日信息");
+
         dutyService.createDutyByGroup(req.getNumber(), req.getArea(), req.getDateTime(), req.getArrangerId(), req.getClubId(), req.getIsMixed(), req.getGroupName());
         return new Response<>(ServiceStatus.SUCCESS).statusText("创建成功");
     }
 
 
     @Operation(summary = "根据userid，添加值日信息")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/create")
     Object createDutyByNameAndClubId(@Valid @RequestBody DutyInfoReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能添加值日信息");
+
         dutyService.createDuty(req.getNumber(), req.getArea(), req.getDateTime(), req.getArrangerId(), req.getCleanerId(), req.getClubId(), req.getIsMixed());
         return new Response<>(ServiceStatus.SUCCESS).statusText("创建成功");
     }
 
     @Operation(summary = "根据小组名称以及社团id，删除值日信息")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/delete_by_group")
     Object deleteDutyByNameAndClubId(@Valid @RequestBody DutyDeleteGroupReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能删除值日信息");
+
         dutyService.deleteDutyAllByGroup(req.getDateTime(), req.getGroupName(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("删除成功");
     }
 
     @Operation(summary = "根据userid，删除值日信息")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/duty/delete")
     Object deleteDutyByNameAndClubId(@Valid @RequestBody DutyDeleteReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能删除值日信息");
+
         dutyService.deleteDutyByUser(req.getDateTime(), req.getCleanerId(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("删除成功");
     }
 
     @Operation(summary = "上传值日照片")
+    @SaCheckRole(AuthRole.CLUB_MEMBER)
     @PostMapping("/club/duty/report_result")
     Object uploadDutyPicture(@Valid @RequestBody DutyFileUpload req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubMember(arrangerId, req.getClubId(), "只有社团成员能上传值日照片");
+
         dutyService.uploadDutyPicture(req.getDateTime(), req.getMemberId(), req.getClubId(), req.getFile());
         return new Response<>(ServiceStatus.SUCCESS).statusText("上传成功");
     }
 
     @Operation(summary = "查询社团值日情况")
+    @SaCheckRole(AuthRole.CLUB_MEMBER)
     @PostMapping("/club/duty/select")
     Object selectDuty(@Valid @RequestBody DutySelectReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubMember(arrangerId, req.getClubId(), "只有社团成员能查询社团值日情况");
 
         DutyInfoQO newQO = new DutyInfoQO(req.getClubId(), req.getNumber(), req.getName(), req.getPageNum(), req.getSize());
         PageVO<DutyInfoVO> result = req.getName().isEmpty() ?
-                (req.getNumber() == null || req.getNumber().isEmpty() ?
+                (req.getNumber().isBlank() || req.getNumber().isEmpty() ?
                         dutyService.selectDuty(newQO) :
                         dutyService.selectDutyByName(newQO)) :
-                (req.getNumber() == null || req.getNumber().isEmpty() ?
+                (req.getNumber().isBlank() || req.getNumber().isEmpty() ?
                         dutyService.selectDutyByNumber(newQO) :
                         dutyService.selectDutyByNumberAndName(newQO));
         //todo 合并
@@ -102,23 +140,31 @@ public class DutyController {
     }
 
     @Operation(summary = "查询社团值日小组")
+    @SaCheckRole(AuthRole.CLUB_MEMBER)
     @PostMapping("/club/duty/group/select")
     Object selectDutyGroup(@Valid @RequestBody DutyGroupSelectReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubMember(arrangerId, req.getClubId(), "只有社团成员能查询社团值日小组");
+
         DutyGroupQO newQO = new DutyGroupQO(req.getClubId(), req.getGroupName(), req.getName(), req.getPageNum(), req.getSize());
         PageVO<TblDutyGroup> result = req.getName().isEmpty() ?
-                (req.getGroupName() == null || req.getGroupName().isEmpty() ?
+                (req.getGroupName().isBlank() || req.getGroupName().isEmpty() ?
                         dutyGroupService.selectDutyGroup(newQO) :
                         dutyGroupService.selectDutyGroupByName(newQO)) :
-                (req.getGroupName() == null || req.getGroupName().isEmpty() ?
+                (req.getGroupName().isBlank() || req.getGroupName().isEmpty() ?
                         dutyGroupService.selectDutyGroupByGroupName(newQO) :
                         dutyGroupService.selectDutyGroupByGroupNameAndName(newQO));
         //todo 合并
         return new Response<>(ServiceStatus.SUCCESS).statusText("查询成功").data(result);
     }
 
-    @Operation(summary = "查询自动值日")
+    @Operation(summary = "查询是否开启自动安排值日功能")
+    @SaCheckRole(AuthRole.CLUB_MEMBER)
     @PostMapping("/club/duty/auto_duty")
     Object selectAutoDuty(@Valid @NotNull @RequestParam @JsonProperty("club_id") Long clubId) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubMember(arrangerId, clubId, "只有社团成员能是否开启自动安排值日功能");
+
         TblDutyCirculation result = dutyCirculationService.selectCirculationByClubId(clubId);
         return new Response<>(ServiceStatus.SUCCESS).statusText("查询成功").data(result);
     }
--- src/main/java/team/project/module/club/personnelchanges/internal/controller/TblUserClubController.java
+++ src/main/java/team/project/module/club/personnelchanges/internal/controller/TblUserClubController.java
@@ -1,5 +1,7 @@
 package team.project.module.club.personnelchanges.internal.controller;
 
+import cn.dev33.satoken.annotation.SaCheckRole;
+import cn.dev33.satoken.stp.StpUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.project.base.controller.response.Response;
 import team.project.base.service.status.ServiceStatus;
+import team.project.module.auth.export.model.enums.AuthRole;
+import team.project.module.auth.export.service.AuthServiceI;
 import team.project.module.club.personnelchanges.internal.model.query.ClubMemberInfoQO;
 import team.project.module.club.personnelchanges.internal.model.request.ClubMemberInfoReq;
 import team.project.module.club.personnelchanges.internal.model.request.UserClubReq;
@@ -24,50 +28,80 @@ public class TblUserClubController {
     @Autowired
     TblUserClubService ucService;
 
+    @Autowired
+    AuthServiceI authService;
     @Operation(summary = "基地设置负责人")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/set_manager")
     Object setManager(@Valid @RequestBody UserClubReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能设置负责人");
+
         ucService.setClubManager(req.getUserId(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("设置成功");
     }
 
     @Operation(summary = "基地撤销负责人")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/unset_manager")
     Object quashManager(@Valid @RequestBody UserClubReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能基地撤销负责人");
+
         ucService.quashClubManager(req.getUserId(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("撤销成功");
     }
 
     @Operation(summary = "添加基地成员")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/add")
     Object createMember(@Valid @RequestBody UserClubReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能添加基地成员");
+
         ucService.createMember(req.getUserId(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("添加成功");
     }
 
     @Operation(summary = "撤销基地成员")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/del")
     Object quashMember(@Valid @RequestBody UserClubReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能撤销基地成员");
+
         ucService.quashMember(req.getUserId(), req.getClubId());
         return new Response<>(ServiceStatus.SUCCESS).statusText("撤销成功");
     }
 
     @Operation(summary = "查询基地所有成员信息")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/select_all")
     Object selectMember(@Valid @RequestBody ClubMemberInfoReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能查询基地所有成员信息");
+
         ClubMemberInfoQO QO = new ClubMemberInfoQO(req.getDepartmentId(), req.getName(), req.getClubId(), req.getPageNum(), req.getSize());
         return new Response<>(ServiceStatus.SUCCESS).statusText("查询成功").data(ucService.selectClubMemberInfo(QO));
     }
 
     @Operation(summary = "查询该用户是否是该社团成员")
+    @SaCheckRole(AuthRole.CLUB_MANAGER)
     @PostMapping("/club/member/select_member")
     Object selectMember(@Valid @RequestBody UserClubReq req) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireClubManager(arrangerId, req.getClubId(), "只有社团负责人能查询该用户是否是该社团成员");
+
         return new Response<>(ServiceStatus.SUCCESS).statusText("查询成功").data(ucService.selectTheMember(req.getUserId(), req.getClubId()));
     }
 
     @Operation(summary = "查询该用户在所有社团的身份")
+    @SaCheckRole(AuthRole.SUPER_ADMIN)
     @PostMapping("/club/member/select_member_all_club_info")
     Object selectMemberAllClubInfo(@NotNull(message = "学号工号不能为空")  @UserIdConstraint @RequestParam("user_id") String userId) {
+        String arrangerId = (String)( StpUtil.getLoginId() );
+        authService.requireSuperAdmin(arrangerId, "只有超级管理员能查询基地所有成员信息");
+
         return new Response<>(ServiceStatus.SUCCESS).statusText("查询成功").data(ucService.selectMemberAllClubInfo(userId));
     }
 }
