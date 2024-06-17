59076b8944292bc5358069120a3bedfc529f50bf
szdxdxdx <3510745357@qq.com>
2024-06-17 14:06:08 +0800

    cloud？

--- src/main/java/team/project/module/club/announcement/config/AnnConfig.java
+++ src/main/java/team/project/module/club/announcement/config/AnnConfig.java
@@ -2,12 +2,12 @@ package team.project.module.club.announcement.config;
 
 import team.project.module.util.filestorage.export.model.enums.FileStorageType;
 
-import static team.project.module.util.filestorage.export.model.enums.FileStorageType.LOCAL;
+import static team.project.module.util.filestorage.export.model.enums.FileStorageType.CLOUD;
 
 public class AnnConfig {
-    public static final FileStorageType STORAGE_TYPE_FILE  = LOCAL;
-    public static final FileStorageType STORAGE_TYPE_ANN   = LOCAL;
-    public static final FileStorageType STORAGE_TYPE_DRAFT = LOCAL;
+    public static final FileStorageType STORAGE_TYPE_FILE  = CLOUD;
+    public static final FileStorageType STORAGE_TYPE_ANN   = CLOUD;
+    public static final FileStorageType STORAGE_TYPE_DRAFT = CLOUD;
     public static final String FOLDER_FILE  = "/club/announcement/file";
     public static final String FOLDER_ANN   = "/club/announcement/announcement";
     public static final String FOLDER_DRAFT = "/club/announcement/announcement";
