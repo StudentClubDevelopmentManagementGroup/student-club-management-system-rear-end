1f0acdebe56d0e3e20490d9a40a0fdc749bfedb8
lin <1980634531@qq.com>
2025-04-10 21:52:13 +0800

    简介完成未测试版

--- src/main/java/team/project/module/club/announcement/internal/config/AnnConfig.java
+++ src/main/java/team/project/module/club/announcement/internal/config/AnnConfig.java
@@ -2,12 +2,12 @@ package team.project.module.club.announcement.internal.config;
 
 import team.project.module.util.filestorage.export.model.enums.FileStorageType;
 
-import static team.project.module.util.filestorage.export.model.enums.FileStorageType.CLOUD;
+import static team.project.module.util.filestorage.export.model.enums.FileStorageType.LOCAL;
 
 public class AnnConfig {
-    public static final FileStorageType STORAGE_TYPE_FILE  = CLOUD;
-    public static final FileStorageType STORAGE_TYPE_ANN   = CLOUD;
-    public static final FileStorageType STORAGE_TYPE_DRAFT = CLOUD;
+    public static final FileStorageType STORAGE_TYPE_FILE  = LOCAL;
+    public static final FileStorageType STORAGE_TYPE_ANN   = LOCAL;
+    public static final FileStorageType STORAGE_TYPE_DRAFT = LOCAL;
     public static final String FOLDER_FILE  = "/club/announcement/file";
     public static final String FOLDER_ANN   = "/club/announcement/announcement";
     public static final String FOLDER_DRAFT = "/club/announcement/announcement";
